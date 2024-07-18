a9bcc60867dc8cb8d005f2ee6cb8626555c95840
Joshua Hu <Z10riderkick@gmail.com>
2024-07-19 01:02:02 +0800

    git push case_part1.py

--- case_part1.py
+++ case_part1.py
@@ -1,13 +1,26 @@
 #import Flask
-from flask import Flask
+
+from flask import Flask, json, redirect, url_for, session
 from requests import request
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
+@app.before_request
 def index():
-    response = request('GET', "https://api.chucknorris.io/jokes/random/")
-    return response.json()["value"]
+    if session['response'] == None:
+        Jokes = []
+        for i in range(10):
+            Jokes.append(request('GET', "https://api.chucknorris.io/jokes/random/").json()["value"])
+        session['response'] = Jokes
+
+@app.route('/getjokes', methods= ['GET', 'POST'], endpoint = "getjokes")
+def getjokes():
+    Jokes = session['response']
+    message = ""
+    for i in Jokes:
+        message = message + f'<li>{i}</li>'
+    return message
 
 if __name__ == "__main__":
     app.run()
