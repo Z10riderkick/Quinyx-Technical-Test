931c4f861e7700d67f79e58f3fa30ad6ca319d06
Joshua Hu <Z10riderkick@gmail.com>
2024-07-19 22:26:19 +0800

    case2

--- case_part2.py
+++ case_part2.py
@@ -2,6 +2,9 @@ import xlrd
 from datetime import datetime
 import csv
 
+#15min constant
+CONST_15min = 60*15
+
 def main():
     #the newest version of xlrd no longer supporting xlsx file
     data = xlrd.open_workbook("data.xls")
@@ -14,25 +17,32 @@ def main():
         
         #find the nearest time bucket
         if rx == 1:
-            nearest = (time//(60*15))*(60*15)
+            nearest = (time//CONST_15min)*CONST_15min
             start = nearest
-            timeslot = start + (60*15)
+            timeslot = start + CONST_15min
         
         #calculate the amount    
-        if time <= timeslot:
+        if time < timeslot:
             transaction = transaction + 1
             sales = sales + sh.row(rx)[1].value
         
         #save & reset    
-        if time > timeslot:
+        if time >= timeslot:
             dict.append((datetime.fromtimestamp(start).strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "{:.2f}".format(sales), transaction))
             sales = sh.row(rx)[1].value
             transaction = 1
-            start = timeslot
-            timeslot = start + (60*15)
+            start = (time//CONST_15min)*CONST_15min
+            
+            #fix the gap
+            timeslot = timeslot + CONST_15min
+            while (timeslot < start):
+                dict.append((datetime.fromtimestamp(timeslot).strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "{:.2f}".format(0), 0))
+                timeslot = timeslot + CONST_15min
+                
+    dict.append((datetime.fromtimestamp(start).strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "{:.2f}".format(sales), transaction))
     
     #save in csv file   
-    with open("case2_data.csv", 'w') as csvfile:
+    with open("case2_data.csv", 'w', newline='') as csvfile:
         csv_out=csv.writer(csvfile)
         csv_out.writerow(['Time', 'Sales', 'Transactions'])
         for data in dict:
